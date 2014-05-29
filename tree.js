31df5f0cebc3f75ed132334d899a19cdd449b52b
SharikovVladislav <sharikov.vladislav@gmail.com>
2014-05-29 21:22:42 +0400

    Updated

--- tree.js
+++ tree.js
@@ -16,32 +16,50 @@ function alphabeta(node, depth, alpha, beta, isMax, g) {
     g.nodes[node.name].shape.items['0'].attr('fill', 'green');
 
     if((depth == 0) || (node.isTerminal == true)) {
+		console.log('getting value from terminal node '+node.name+' (value is '+node.value+')');
         return node.value;
     }
     if(isMax) {
-        console.log('maximizing');
+		
+        console.log('maximizing ('+node.name+')');
         for (var i in node.children) {
             var child = node.children[i];
-            console.log(g.nodes[child.name]);
+            //console.log(g.nodes[child.name]);
             alpha = Math.max(alpha, alphabeta(child, depth-1, alpha, beta, false, g));
+			console.log('alpha value is set to '+alpha);
             if(beta <= alpha) {
-                console.log('beta '+beta+' alpha '+alpha);
+                //console.log('beta '+beta+' alpha '+alpha);
+				console.log('beta cut-off ('+beta+'<='+alpha+'), others children wouldn\'t be visited');
                 break;
             }
         }
+        g.nodes[node.name].shape.items['1'].attr('text', alpha);
+		g.nodes[node.name].shape.items['0'].attr('fill', 'blue');
 
+		console.log('returning alpha, node '+node.name+' value is set as '+node.value);
+		console.log('going back to node '+g.nodes[node.name].parent)
         return alpha;
     } else {
-        console.log('minimizing');
+		
+        console.log('minimizing ('+node.name+')');
         for (var i in node.children) {
             var child = node.children[i];
-            console.log(g.nodes[child.name]);
-            beta = Math.min(beta, alphabeta(child, depth-1, alpha, beta, true, g));
+            //console.log(g.nodes[child.name]);
+			var childValue = alphabeta(child, depth-1, alpha, beta, true, g);
+            beta = Math.min(beta, childValue);
+			
+			console.log('beta value is set to '+beta);
             if (beta <= alpha) {
-                console.log('beta '+beta+' alpha '+alpha);
+                //console.log('beta '+beta+' alpha '+alpha);
+				console.log('alpha cut-off ('+beta+'<='+alpha+'), others children wouldn\'t be visited');
                 break;
             }
         }
+        g.nodes[node.name].shape.items['1'].attr('text', beta);
+		g.nodes[node.name].shape.items['0'].attr('fill', 'red');
+		
+		console.log('returning beta, node '+node.name+' value is set as '+node.value);
+		console.log('going back to node '+g.nodes[node.name].parent);
         return beta;
     }
 }
